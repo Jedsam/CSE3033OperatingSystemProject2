4de8ab1c9b9e640cb57e099736d56e484d1922e6
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2023-12-18 18:09:20 +0300

    Bookmark done.

--- mainSetup.c
+++ mainSetup.c
@@ -147,74 +147,68 @@ in the next command line; separate it into distinct arguments (using blanks as
 delimiters), and set the args array entries to point to the beginning of what
 will become null-terminated, C-style strings. */
 
-void setup(char inputBuffer[], char *args[],int *background)
-{
-    int length, /* # of characters in the command line */
-        i,      /* loop index for accessing inputBuffer array */
-        start,  /* index where beginning of next command parameter is */
-        ct;     /* index of where to place the next parameter into args[] */
-    
-    ct = 0;
-        
-    /* read what the user enters on the command line */
-    length = read(STDIN_FILENO,inputBuffer,MAX_LINE);  
+void setup(char inputBuffer[], char *args[], int *background) {
+    int length;
+    int i;
+    int start = -1;
+    int ct = 0;
+    int inQuote = 0; // Flag to indicate if we are inside quotes
 
-    /* 0 is the system predefined file descriptor for stdin (standard input),
-       which is the user's screen in this case. inputBuffer by itself is the
-       same as &inputBuffer[0], i.e. the starting address of where to store
-       the command that is read, and length holds the number of characters
-       read in. inputBuffer is not a null terminated C-string. */
+    length = read(STDIN_FILENO, inputBuffer, MAX_LINE);
 
-    start = -1;
     if (length == 0)
-        exit(0);            /* ^d was entered, end of user command stream */
+        exit(0);
 
-/* the signal interrupted the read system call */
-/* if the process is in the read() system call, read returns -1
-  However, if this occurs, errno is set to EINTR. We can check this  value
-  and disregard the -1 value */
-    if ( (length < 0) && (errno != EINTR) ) {
+    if (length < 0 && errno != EINTR) {
         perror("error reading the command");
-	exit(-1);           /* terminate with error code of -1 */
+        exit(-1);
     }
 
-	printf(">>%s<<",inputBuffer);
-    for (i=0;i<length;i++){ /* examine every character in the inputBuffer */
+    for (i = 0; i < length; i++) {
+        switch (inputBuffer[i]) {
+            case ' ':
+            case '\t':
+                if (inQuote == 0) {
+                    if (start != -1) {
+                        args[ct] = &inputBuffer[start];
+                        ct++;
+                    }
+                    inputBuffer[i] = '\0';
+                    start = -1;
+                }
+                break;
 
-        switch (inputBuffer[i]){
-	    case ' ':
-	    case '\t' :               /* argument separators */
-		if(start != -1){
-                    args[ct] = &inputBuffer[start];    /* set up pointer */
-		    ct++;
-		}
-                inputBuffer[i] = '\0'; /* add a null char; make a C string */
-		start = -1;
-		break;
+            case '\"':
+                if (inQuote) {
+                    inQuote = 0;
+                } else {
+                    inQuote = 1;
+                    start = i + 1; // Start of the quoted string
+                }
+                break;
 
-            case '\n':                 /* should be the final char examined */
-		if (start != -1){
-                    args[ct] = &inputBuffer[start];     
-		    ct++;
-		}
+            case '\n':
+                if (start != -1) {
+                    args[ct] = &inputBuffer[start];
+                    ct++;
+                }
                 inputBuffer[i] = '\0';
-                args[ct] = NULL; /* no more arguments to this command */
-		break;
+                args[ct] = NULL;
+                break;
 
-	    default :             /* some other character */
-		if (start == -1)
-		    start = i;
-                if (inputBuffer[i] == '&'){
-		    *background  = 1;
-                    inputBuffer[i-1] = '\0';
-		}
-	} /* end of switch */
-     }    /* end of for */
-     args[ct] = NULL; /* just in case the input line was > 80 */
+            default:
+                if (start == -1 && inQuote == 0)
+                    start = i;
+                if (inputBuffer[i] == '&' && inQuote == 0) {
+                    *background = 1;
+                    inputBuffer[i] = '\0';
+                }
+        }
+    }
+    args[ct] = NULL; // Null-terminate the argument list
+}
 
-	for (i = 0; i <= ct; i++)
-		printf("args %d = %s\n",i,args[i]);
-} /* end of setup routine */
+
  int checkPID(pid_t *childpidList, int length){
     int i;
     int status;
@@ -403,13 +397,26 @@ void handleSignal(int sig) {
 }
 
 void manageBookmark(char *args[]) {
+    if (args[1] == NULL) {
+        printf("No arguments provided for bookmark command.\n");
+        return;
+    }
+
     if (strcmp(args[1], "-l") == 0) {
         // List bookmarks
-        for (int i = 0; i < bookmarkCount; i++) {
-            printf("%d: %s\n", i, bookmarks[i]);
+        if (bookmarkCount == 0) {
+            printf("No bookmarks set.\n");
+        } else {
+            for (int i = 0; i < bookmarkCount; i++) {
+                printf("%d: %s\n", i, bookmarks[i]);
+            }
         }
     } else if (strcmp(args[1], "-d") == 0) {
         // Delete a bookmark
+        if (args[2] == NULL) {
+            printf("No index provided for deletion.\n");
+            return;
+        }
         int index = atoi(args[2]);
         if (index >= 0 && index < bookmarkCount) {
             free(bookmarks[index]);
@@ -417,16 +424,21 @@ void manageBookmark(char *args[]) {
                 bookmarks[i] = bookmarks[i + 1];
             }
             bookmarkCount--;
+            printf("Bookmark %d deleted.\n", index);
+        } else {
+            printf("Invalid index for bookmark deletion.\n");
         }
     } else {
         // Add a new bookmark
         if (bookmarkCount < MAX_BOOKMARKS) {
             bookmarks[bookmarkCount++] = strdup(args[1]);
+            printf("Bookmark added: %s\n", args[1]);
         } else {
             printf("Bookmark limit reached.\n");
         }
     }
 }
+
 int main(void) {
     char inputBuffer[MAX_LINE]; /* Buffer to hold the command entered */
     int background; /* Equals 1 if a command is followed by '&' */
